75441ad17bd38efcd96ef50e9c7e51649b85e368
Sufal Shirodkar <sufal.s@nhance.ai>
2026-05-06 11:49:38 +0530

    created express server containerized it on docker using docker compose file

---
+++ README.md
@@ -0,0 +1,1 @@
+# docker-epress-app
---
+++ app.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('Connected to MongoDB')
+
+        app.listen(PORT, () => {
+            console.log(`Server Is Running On Port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+startServer()
+
+app.use('/', (req, res) => {
+    return res.json("Hello world from Docker !!")
+})
+
